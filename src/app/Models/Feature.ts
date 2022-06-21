9c11ded66b2ee1dc3c54d2bbef93e62ba081b63b
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-06-21 13:42:23 -0300

    Criação de model com features

---
+++ src/app/Models/Feature.ts
@@ -0,0 +1,11 @@
+export class Feature {
+    titulo:string; 
+    descricao:string; 
+    urlImg:string;
+
+    constructor(titulo:string, descricao:string, urlImg:string){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.urlImg = urlImg;
+    }
+}
