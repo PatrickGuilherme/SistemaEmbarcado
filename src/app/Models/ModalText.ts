7b4df084d6217774c0fcb337f66f4821b01b9913
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-06-22 15:35:28 -0300

    Criação de modal para texto no componente feature

---
+++ src/app/Models/ModalText.ts
@@ -0,0 +1,11 @@
+export class ModalText {
+    titulo:string; 
+    descricao:string; 
+    ano:number;
+
+    constructor(titulo:string, descricao:string, ano:number){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.ano = ano;
+    }
+}
