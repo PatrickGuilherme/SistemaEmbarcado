bd5ab96538c0bd231d603f7c402a1188f818f716
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-06-27 13:37:54 -0300

    Modal info

--- src/app/Components/modal-info/modal-info.component.ts
+++ src/app/Components/modal-info/modal-info.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, TemplateRef, ViewChild } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ModalText } from 'src/app/Models/ModalText';
 
 @Component({
-  selector: 'app-modal-info',
+  selector: 'app-modal',
   templateUrl: './modal-info.component.html',
   styleUrls: ['./modal-info.component.css']
 })
-export class ModalInfoComponent implements OnInit {
+export class ModalInfoComponent implements OnChanges{
   modalRef?: BsModalRef;
+  @Input() ModalText:ModalText;
+  @Input() switchModal:boolean;
+  @ViewChild('template') public template: TemplateRef<any>;
+
   constructor(private modalService: BsModalService) {}
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['ModalText'].firstChange == false){
+      if(changes['ModalText'].currentValue){
+        this.ShowModal();
+      }
+    }
   }
 
-  public ShowModal(template: TemplateRef<any>){
-    this.modalRef = this.modalService.show(template);
+  public ShowModal() {
+    this.modalRef = this.modalService.show(this.template);
   }
 
-  public CloseModal(){
+  public CloseModal() {
     this.modalRef?.hide();
   }
 }
