19c6e71cfc11cbaf2ee88ce9f1103a030a6b6404
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-06-27 13:41:10 -0300

    Atualização de componente arquitetura

--- src/app/Components/architecture/architecture.component.ts
+++ src/app/Components/architecture/architecture.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./architecture.component.css']
 })
 export class ArchitectureComponent implements OnInit {
+  public text:string;
+  public title:string;
 
-  constructor() { }
+  constructor() {
+    this.title = "Microcontroladores";
+    this.text =  "Quando se analisa o desenvolvimento de sistemas embarcados, é comum que o cerne do projeto seja um microcontrolador, este possui um processador dedicado a uma finalidade específica. Em um microcontrolador geralmente as entradas são fornecidas ao sistema e estas geram as as saídas. Neste contexto as entradas e saídas representam elementos como: sensores, atuadores, dispositivos de comunicação, dispositivos de interface gráfica, entre outros.";       
+  }
 
   ngOnInit(): void {
   }
