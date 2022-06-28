2fef7150b7403324a5481365916afebeac4c4484
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-06-28 18:43:19 -0300

    Adição de referencias aos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './Components/main-page/main-page.component';
@@ -12,6 +10,10 @@ import { FeaturesComponent } from './Components/features/features.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ModalInfoComponent } from './Components/modal-info/modal-info.component';
 import { MicrocontrollerComponent } from './Components/microcontroller/microcontroller.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ApplicationsComponent } from './Components/applications/applications.component';
+import { BannerMercadoComponent } from './Components/banner-mercado/banner-mercado.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +25,15 @@ import { MicrocontrollerComponent } from './Components/microcontroller/microcont
     TimeLineComponent,
     FeaturesComponent,
     ModalInfoComponent,
-    MicrocontrollerComponent
+    MicrocontrollerComponent,
+    ApplicationsComponent,
+    BannerMercadoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ModalModule.forRoot(),
+    CarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
