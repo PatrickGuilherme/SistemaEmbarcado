3f8053fa5293c461ac323cef54fa88db9fa70ae6
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-06-28 18:42:31 -0300

    Criação de looping para exibir o microcontrolador

--- src/app/Components/microcontroller/microcontroller.component.ts
+++ src/app/Components/microcontroller/microcontroller.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./microcontroller.component.css']
 })
 export class MicrocontrollerComponent implements OnInit {
+  public contL:number = 20;
+  public contC:number = 10;
 
   constructor() { }
 
